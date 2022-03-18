9426c8b1bd92bf15a4b10b7d39e79bfb001c0d60
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-03-18 22:19:51 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blueGrey,
+      ),
+      title: "WhatsApp",
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color.fromRGBO(7, 94, 84, 1),
+            elevation: 10,
+            titleSpacing: 20,
+
+            title: Text(
+              "WhatsApp",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22,
+              ),
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(
+                  Icons.search,
+                  size: 25,
+                ),
+                color: Colors.white,
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.more_vert,
+                  size: 25,
+                ),
+                color: Colors.white,
+                onPressed: () {},
+              ),
+            ],
+
+            // centerTitle: true,
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(7, 94, 84, 1),
+                    Color.fromRGBO(7, 94, 84, 1)
+                  ],
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topLeft,
+                ),
+              ),
+            ),
+            bottom: TabBar(
+              indicatorColor: Colors.white,
+              // isScrollable: true,
+              indicatorWeight: 2,
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.camera_alt),
+                ),
+                Tab(
+                  text: "CHATS",
+                ),
+                Tab(
+                  text: "STATUS",
+                ),
+                Tab(
+                  text: "CALLS",
+                ),
+              ],
+            ),
+          ),
+          body: Container(),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Color.fromRGBO(37, 211, 102, 1),
+            onPressed: () {},
+            tooltip: 'Increment',
+            child: Icon(Icons.message_sharp),
+          ),
+        ),
+      ),
+    );
+  }
+}
